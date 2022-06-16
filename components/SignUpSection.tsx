9b8fd39fd9b69704ec388bf8c56ae3d837ab29d7
Samson Zhang <wwsamson@yahoo.com>
2022-06-16 15:33:13 -0400

    add sign up form link

--- components/SignUpSection.tsx
+++ components/SignUpSection.tsx
@@ -4,7 +4,7 @@ export default function SignUpSection() {
     return (
         <Section className="sm:flex items-center justify-center">
             <p className="text-xl sm:text-2xl uppercase font-bold mr-6">pilot cohort<br className="sm:hidden"/> <span className="text-brand"><span className="font-black">nyc</span> starting june 27</span></p>
-            <a href="" className="inline-block font-display text-white bg-brand px-10 py-3 text-xl font-medium mt-4 sm:mt-0">sign up</a>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSeTjlQbaDD_kHhymRfJISfz22uhGltvpDP8ZRwG_sabb5qwVw/viewform?usp=sf_link" className="inline-block font-display text-white bg-brand px-10 py-3 text-xl font-medium mt-4 sm:mt-0">sign up</a>
         </Section>
     )
 }
